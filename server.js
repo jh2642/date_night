56cb1e6f943ce972bfacae7f05ddca8a67b7e4f0
James Hildreth <hildreth.james@gmail.com>
2016-08-16 14:22:45 -0400

    added viespblic

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 8080))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
     res.send('hello world')
 })
 
-app.listen(8080, function () {
-    console.log('Listening to port 8080 !')
+app.listen(app.get('port'), function () {
+    console.log('Listening to port 8080 !', app.get('port'))
 })
