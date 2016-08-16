acc3e8a0d8253604b15bcd426fa22b6aefe14b6e
James Hildreth <hildreth.james@gmail.com>
2016-08-16 14:06:27 -0400

    added server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hi there")
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+app.listen(80, function () {
+    console.log('Listening to port 80 !')
+})
