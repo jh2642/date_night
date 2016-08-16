48849be912dee63b2ea12ccc810dd37ae676c0fd
James Hildreth <hildreth.james@gmail.com>
2016-08-16 15:22:21 -0400

    added postgres

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs')
 
 var knex = require('knex') ( {
     client: 'pg',
-    connection: (process.env.PG_CONNECTION_STRING) || ('postgres://localhost/jh2642'),
+    connection: (process.env.PG_CONNECTION_STRING || 'postgres://localhost/jh2642'),
     searchPath: 'knex,public'
 })
 app.get('/install', function( request, response) {
