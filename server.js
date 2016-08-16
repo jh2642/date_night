db1fe1ff00a3bfe713564ae9ece0a0f572e79e85
James Hildreth <hildreth.james@gmail.com>
2016-08-16 14:29:56 -0400

    added changes to js file

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require('express')
+var ejs = require('ejs')
 var app = express()
+
 
 app.set('port', (process.env.PORT || 8080))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-    res.send('hello world')
+app.get('/', function (request, respond) {
+    ejs.renderFile('index.ejs', {data: 'testing 123'}, {}, function(err, html) {
+        response.send(html)
+    })
 })
+
+app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
     console.log('Listening to port 8080 !', app.get('port'))
