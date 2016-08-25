d66583285a285202f8cf0be4a86645958d6bd77b
James Hildreth <hildreth.james@gmail.com>
2016-08-25 13:53:14 -0400

    server work

--- public/js/googlesignin.js
+++ public/js/googlesignin.js
@@ -29,6 +29,9 @@ function onSignIn(googleUser) {
 
     fetch(api+'/users/create', {
         method: 'POST',
+        headers: {
+          'Content-Type': 'application/*+json'
+        },
         body: JSON.stringify({
             name: profile.getName(),
             image_url: profile.getImageUrl(),
