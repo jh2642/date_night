9508738ceef15a91c35a7a0bf5e24767ea554c6e
James Hildreth <hildreth.james@gmail.com>
2016-08-27 11:16:21 -0400

    sv wk

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -29,7 +29,6 @@ function checkAuth() {
             // Hide auth UI, then load client library.
             authorizeDiv.style.display = 'none';
             //get profile information from db
-            window.addEventListener('googlesignin', function() {
                 fetch(api+'/users/profile?id=' + user_id, {
                     method: 'GET',
                     credentials: 'include',
@@ -57,7 +56,6 @@ function checkAuth() {
 
                     loadCalendarApi();
                 })
-            })
         }
         else {
             // Show auth UI, allowing the user to initiate authorization by
