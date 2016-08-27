62d5ab9a2f3757be85e3db1e4c0778c1645fe211
James Hildreth <hildreth.james@gmail.com>
2016-08-27 14:01:02 -0400

    sv wk

--- public/js/main.js
+++ public/js/main.js
@@ -260,7 +260,10 @@ document.getElementById('grabDetails').addEventListener('click', function() {
                 return response.json()
             })
             .then(function(selectedVenue) {
-                console.log(selectedVenue.result) //returns objects
+                selectedVenue.result.forEach(function(item) {
+                    console.log(item)
+                })
+                // console.log(selectedVenue.result) //returns objects
                 locationType = selectedVenue.result.types[0]
                 locationName = selectedVenue.result.name
                 locationAddress = selectedVenue.result.formatted_address
@@ -268,7 +271,7 @@ document.getElementById('grabDetails').addEventListener('click', function() {
                 locationUrl = selectedVenue.result.url
 
                 eachLoc = locationName + ', ' + locationAddress + ', ' + locationPhoneNumber
-                console.log(eachLoc)
+                // console.log(eachLoc)
                 document.getElementById('dateLoc1').innerHTML = eachLoc;
                 document.getElementById('dateTime').innerHTML = moment(document.getElementById('startTime').value).format('LLLL');
 
