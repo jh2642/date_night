7558c11d1b76bb29a17bc60c9a8d49e9c89aeb0c
James Hildreth <hildreth.james@gmail.com>
2016-08-27 16:02:06 -0400

    sv wk

--- public/js/profile.js
+++ public/js/profile.js
@@ -56,7 +56,7 @@ window.addEventListener('googlesignin', function() {
     })
 
 })
-document.getElementById('addDateInfo').addEventListener('click', function() {
+document.getElementById('scheduleReminder').addEventListener('click', function() {
     console.log('profile.js')
 })
 document.getElementById("dateReminder").flatpickr();
