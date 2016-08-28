a2f754484b4e05d3e6d37f90c43d4be7cb5f3947
James Hildreth <hildreth.james@gmail.com>
2016-08-27 21:52:57 -0400

    database post and fetch

--- public/js/profile.js
+++ public/js/profile.js
@@ -200,7 +200,8 @@ function checkAuth() {
 
             $(document).ready(function(){
                 $('body').on('click', '.calendar-id', function(){
-
+                    var id = $(this).attr('calendar-id')
+                    console.log(id)
                     gapi.client.load('calendar', 'v3', deleteEvents);
                 });
                 // document.getElementById('updateEvents').addEventListener('click', function() {
@@ -268,8 +269,6 @@ function checkAuth() {
 
                 //delete event here
                 function deleteEvents() {
-                    var id = $(this).attr('calendar-id')
-                    console.log(id)
 
                     var request3 = gapi.client.calendar.events.delete({
                         calendarId: 'primary',
