046c0bf9e84bcf2d5d6290a99770bdaa5601c4d0
James Hildreth <hildreth.james@gmail.com>
2016-08-28 13:17:55 -0400

    database post and fetch

--- public/js/profile.js
+++ public/js/profile.js
@@ -106,7 +106,7 @@ $(document).ready(function(){
         console.log(calId)
 
         fetch(api+'/events/deletedatenight?user_id=' + user_id, {
-            method: 'DELETE',
+            method: 'POST',
             credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
