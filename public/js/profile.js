345af88335216ed157c1af5c4aa799460449d939
James Hildreth <hildreth.james@gmail.com>
2016-08-29 14:05:25 -0400

    logic work

--- public/js/profile.js
+++ public/js/profile.js
@@ -65,7 +65,7 @@ window.addEventListener('googlesignin', function() {
             response.forEach(function(item) {
 
                 var div = document.createElement('div')
-                div.classList.add('dateNightEvent', 'col-md-4')
+                div.classList.add('dateNightEvent')
 
                 var name = document.createElement('h3')
                 name.innerHTML = item.rest_name
