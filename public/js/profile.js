da83471179d27940bc1312d2a1ec1cb353fff365
James Hildreth <hildreth.james@gmail.com>
2016-08-29 11:31:48 -0400

    db work

--- public/js/profile.js
+++ public/js/profile.js
@@ -89,7 +89,7 @@ window.addEventListener('googlesignin', function() {
                 eventDetails.classList.add('btn', 'btn-primary', 'calendar-id')
                 eventDetails.innerHTML = 'delete event'
                 div.appendChild(eventDetails)
-                if(moment(item.event_at).format('LLL') >= moment().format('LT')) {
+                if(moment(item.event_at).format('LLLL') >= moment()) {
                 document.getElementById('calendarEventsDb').appendChild(div)
             }
         })
