346a119bdf1f220ab5e9780748d22496042298d1
James Hildreth <hildreth.james@gmail.com>
2016-08-29 13:19:53 -0400

    logic work

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -123,7 +123,7 @@ function checkAuth() {
                 var emailInstead = document.getElementById('newEmail').value
                 var nameInstead = document.getElementById('newName').value
                 var pickupInfo = document.querySelector('input[name="meetDate"]:checked').value;
-                console.log(pickupInfo)
+            
                 var meetingDate
 
                 if (emailInstead.length) {
@@ -179,7 +179,7 @@ function checkAuth() {
                             rest_id: locationId,
                             rest_name: locationName,
                             date_email: dateEmail,
-                            special_comments: descriptionDate,
+                            special_comments: pickupInfo + descriptionDate,
                             calendar_id: idForEvent
                         })
                     })
