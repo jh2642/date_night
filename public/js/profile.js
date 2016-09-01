820eeacbaa85cfdee84c9367db239de63eba4b4f
James Hildreth <hildreth.james@gmail.com>
2016-09-01 19:42:52 -0400

    profile work

--- public/js/profile.js
+++ public/js/profile.js
@@ -49,7 +49,9 @@ window.addEventListener('googlesignin', function() {
         document.getElementById('googlePic').appendChild(featureImage)
          console.log(response.date_name)
         if(response.date_name === 'null') {
+            .then(function() {
             document.getElementById('dateProfileBox2').classList.remove('hidden')
+            })
         }
         else {
         document.getElementById('dateProfileBox').classList.remove('hidden')
