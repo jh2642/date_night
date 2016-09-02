86981ece977425d8e9d1b7e5ca41fc8eb4f79bf0
James Hildreth <hildreth.james@gmail.com>
2016-09-02 09:58:55 -0400

    profile page

--- public/js/googlesignin.js
+++ public/js/googlesignin.js
@@ -5,6 +5,7 @@ var googlesignin = new Event('googlesignin')
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     var profile = googleUser.getBasicProfile();
+    checkAuth();
     // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     // console.log('Name: ' + profile.getName());
     // console.log('Image URL: ' + profile.getImageUrl());
