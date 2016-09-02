e5699717fe57d1fb5de60a3e867e88a234fc029b
James Hildreth <hildreth.james@gmail.com>
2016-09-02 09:21:21 -0400

    profile page

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -27,7 +27,7 @@ function checkAuth() {
         if (authResult && !authResult.error) {
             // Hide auth UI, then load client library.
             authorizeDiv.style.display = 'none';
-            location.reload();
+            location="/schedule.html"
             loadCalendarApi();
 
             //get profile information from db
