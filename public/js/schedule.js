0ebbf2ef202134047c12b6f65b0fe57fb77c9292
James Hildreth <hildreth.james@gmail.com>
2016-09-01 21:06:53 -0400

    profile work

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -31,7 +31,7 @@ function checkAuth() {
             loadCalendarApi();
 
             //get profile information from db
-            fetch(api+'/users/profile?id=' + user_id, {
+            fetch(api+'/users/profile?id=' + id, {
                 method: 'GET',
                 credentials: 'include',
                 headers: {
