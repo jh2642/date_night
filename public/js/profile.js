447d3e6a4c0076adb7eb6480ec3149371c0b9471
James Hildreth <hildreth.james@gmail.com>
2016-09-01 22:45:53 -0400

    profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -75,7 +75,7 @@ window.addEventListener('googlesignin', function() {
             response.forEach(function(item) {
 
                 var div = document.createElement('div')
-                div.classList.add('dateNightEvent', 'col-sm-6', 'col-md-4', 'text-center')
+                div.classList.add('dateNightEvent', 'col-sm-12', 'col-md-4', 'text-center')
 
                 var name = document.createElement('h3')
                 name.innerHTML = item.rest_name
