0f37281ec6ffe227a250b7d839ce505b98ed5aa0
James Hildreth <hildreth.james@gmail.com>
2016-09-06 17:12:18 -0400

    layout

--- public/js/profile.js
+++ public/js/profile.js
@@ -125,7 +125,7 @@ window.addEventListener('googlesignin', function() {
             return response.json()
         })
         .then(function(response) {
-            console.log(response.rest_name)
+            console.log(response)
 
         //     response.forEach(function(item) {
         //
