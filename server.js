0548f0d55552d12deb0e90dd144440196d084173
James Hildreth <hildreth.james@gmail.com>
2016-10-02 12:46:56 -0400

    adding search function

--- server.js
+++ server.js
@@ -71,7 +71,9 @@ app.get('/api/v1/places', function (request, response) {
 
 //this is the api to get google places data using SEARCH CRITERIA//
 app.get('/api/v1/search', function (request, response) {
-    https.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + request.query.searchTerm + '&key=' + process.env.GOOGLE_PLACES_KEY,
+    // https.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + request.query.searchTerm + '&key=' + process.env.GOOGLE_PLACES_KEY,
+    https.get('https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney' + '&key=' + process.env.GOOGLE_PLACES_KEY,
+
      function (err, data, body) {
         var body = JSON.parse(body)
         body.results = body.results.filter(function(place) {
