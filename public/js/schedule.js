a71cf63d29dcbe3b263b0d08523c01bfb51e6a9d
James Hildreth <hildreth.james@gmail.com>
2016-12-02 11:37:05 -0500

    friend date

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -126,11 +126,11 @@ function checkAuth() {
                 // var dateMessage = document.getElementById('dateMessage').value
 
                 if (isDateGoing === 'yes') {
-                    // dateEmail = dateEmail
+                    dateEmail = dateEmail
                     console.log(isDateGoing)
                 }
-                else (isDategoing === 'no') {
-                    // dateEmail = yourEmail
+                else {
+                    dateEmail = yourEmail
                     console.log(isDateGoing)
                 }
 
