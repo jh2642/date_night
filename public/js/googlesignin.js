dcd8585fde71266064d76e3eaa4e8be2012c703d
James Hildreth <hildreth.james@gmail.com>
2016-12-07 16:41:27 -0500

    color

--- public/js/googlesignin.js
+++ public/js/googlesignin.js
@@ -44,7 +44,7 @@ function onSignIn(googleUser) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+        // console.log('User signed out.');
         //added to redirect to the index page after log out
         location="/index.html"
     });
